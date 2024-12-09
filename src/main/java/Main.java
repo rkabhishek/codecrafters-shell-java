c370de324482b1f4c2533392aa0f3d69c41eb6a8
Abhishek Kaushik <akaushik079@gmail.com>
2024-12-09 23:32:41 +0530

    Implement the builtin exit command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
+
+            if (input.equals("exit 0")) {
+                System.exit(0);
+            }
+
             System.out.println(input + ": command not found");
         }
     }
