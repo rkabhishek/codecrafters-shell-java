cfddbbeb0f313d63602b013f78e487032bb6ebb2
Abhishek Kaushik <akaushik079@gmail.com>
2025-07-06 15:18:43 +0530

    Implement exit builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,10 @@ public class Main {
         String input = scanner.nextLine();
 
         while (!input.isEmpty()) {
+            if (input.equals("exit 0")) {
+                System.exit(0);
+            }
+            
             System.out.println(input + ": command not found");
             System.out.print("$ ");
             input = scanner.nextLine();
